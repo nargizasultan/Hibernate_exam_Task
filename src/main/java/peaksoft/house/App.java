3177cc1bb39e4ad5073c85ba892470363d78cfb5
beksultan <selflearningjava@gmail.com>
2022-04-26 10:41:12 +0600

    Initial commit

---
+++ src/main/java/peaksoft/house/App.java
@@ -0,0 +1,13 @@
+package peaksoft.house;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
